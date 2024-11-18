fddb1fcbcb4edb22ba5f5eaaf2802d8a243d71fa
sreekar <sreekaramrutham@gmail.com>
2024-11-18 23:53:18 +0530

    maven_java_project

---
+++ src/main/java/mavenjava/com/BD1A05A3/App.java
@@ -0,0 +1,13 @@
+package mavenjava.com.BD1A05A3;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
